05397bace0728a1dc2a52c86e42ff454c868b935
Chris Weirup <cweirup@gmail.com>
2020-02-21 23:05:38 -0500

    Basic URL capture and status working. New tab bar button.

--- Joplin Clipper Extension/SafariExtensionViewController.swift
+++ Joplin Clipper Extension/SafariExtensionViewController.swift
@@ -10,10 +10,151 @@ import SafariServices
 
 class SafariExtensionViewController: SFSafariExtensionViewController {
     
+    @IBOutlet weak var pageTitle: NSTextField!
+    @IBOutlet weak var pageUrl: NSTextField!
+    @IBOutlet weak var pageTitleLabel: NSTextField!
+    
     static let shared: SafariExtensionViewController = {
         let shared = SafariExtensionViewController()
         shared.preferredContentSize = NSSize(width:320, height:240)
         return shared
     }()
+    
+    override func viewWillAppear() {
+        print("Entered viewWillAppear()")
+        super.viewWillAppear()
+        checkServerStatus()
+        loadPageInfo()
+    }
+    
+    @IBAction func clipUrl(_ sender: Any) {
+      let joplinEndpoint: String = "http://localhost:41184/notes"
+        NSLog("Entered clipUrl method")
+        
+      guard let joplinURL = URL(string: joplinEndpoint) else {
+        NSLog("Error: cannot create URL")
+        return
+      }
+      var joplinUrlRequest = URLRequest(url: joplinURL)
+      joplinUrlRequest.httpMethod = "POST"
+        
+      let newNote: [String: Any] = ["title": pageTitle.stringValue, "body": pageUrl.stringValue]
+      let jsonNote: Data
+      do {
+        jsonNote = try JSONSerialization.data(withJSONObject: newNote, options: [])
+        joplinUrlRequest.httpBody = jsonNote
+      } catch {
+        NSLog("Error: cannot create JSON from note")
+        return
+      }
+
+      let session = URLSession.shared
+
+      let task = session.dataTask(with: joplinUrlRequest) {
+        (data, response, error) in
+        guard error == nil else {
+          NSLog("error calling POST on /notes")
+          //NSLog(error!)
+          return
+        }
+        guard let responseData = data else {
+          NSLog("Error: did not receive data")
+          return
+        }
+
+        // parse the result as JSON, since that's what the API provides
+        do {
+          guard let receivedNote = try JSONSerialization.jsonObject(with: responseData,
+            options: []) as? [String: Any] else {
+              NSLog("Could not get JSON from responseData as dictionary")
+              return
+          }
+          NSLog("The note is: " + receivedNote.description)
+
+          guard let noteID = receivedNote["id"] as? String else {
+            NSLog("Could not get todoID as int from JSON")
+            return
+          }
+          NSLog("The ID is: \(noteID)")
+        } catch  {
+          NSLog("error parsing response from POST on /notes")
+          return
+        }
+      }
+      task.resume()
+
+    }
+    
+    func loadPageInfo() {
+        SFSafariApplication.getActiveWindow{ (activeWindow) in
+            activeWindow?.getActiveTab{ (activeTab) in
+                activeTab?.getActivePage{ (activePage) in
+                    activePage?.getPropertiesWithCompletionHandler{ (pageProperties) in
+                        DispatchQueue.main.async {
+                            self.pageTitle.stringValue = pageProperties?.title ?? ""
+                            self.pageUrl.stringValue = pageProperties?.url?.absoluteString ?? ""
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    func checkServerStatus() {
+        let joplinEndpoint: String = "http://localhost:41184/ping"
+          NSLog("Entered checkServerStatus method")
+          
+        guard let joplinURL = URL(string: joplinEndpoint) else {
+          NSLog("Error: cannot create URL")
+          return
+        }
+        var joplinUrlRequest = URLRequest(url: joplinURL)
+        joplinUrlRequest.httpMethod = "GET"
+
+        let session = URLSession.shared
+
+        let task = session.dataTask(with: joplinUrlRequest) {
+          (data, response, error) in
+          guard error == nil else {
+            NSLog("error calling GET on /ping. Assume service is not running")
+            //NSLog(error!)
+            self.pageTitleLabel.stringValue = "Server is not running!"
+            return
+          }
+          guard let responseData = data else {
+            NSLog("Error: did not receive data")
+            return
+          }
+        
+          // parse the result as String, since that's what the API provides
+          do {
+            guard let receivedStatus = String(data: responseData, encoding: .utf8) else {
+                NSLog("Count not parse server status from response.")
+                return
+            }
+            NSLog("The response is: " + receivedStatus)
+
+            if receivedStatus == "JoplinClipperServer" {
+                self.pageTitleLabel.stringValue = "Server is running!"
+            } else {
+                self.pageTitleLabel.stringValue = "Server is not running!"
+            }
+            
+            //guard let noteID = receivedNote["id"] as? String else {
+            //  NSLog("Could not get todoID as int from JSON")
+            //  return
+            //}
+            //NSLog("The ID is: \(noteID)")
+          } catch  {
+            NSLog("error parsing response from POST on /notes")
+            return
+          }
+        }
+        task.resume()
+
+        
+        //self.pageTitleLabel.stringValue = pageProperties?.title ?? ""
+    }
 
 }
